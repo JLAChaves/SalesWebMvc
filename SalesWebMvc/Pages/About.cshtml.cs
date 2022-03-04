59656a0ddf605ae55fa7c4abbc46836d39b82673
JLAChaves <lucas2015nxi@gmail.com>
2022-03-04 01:34:21 -0400

    Project created

---
+++ SalesWebMvc/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+        public string Student { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Salles Web MVC App from c# Course";
+            Student = "Student: JLAChaves";
+        }
+    }
+}
