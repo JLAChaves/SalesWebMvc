7431ccd4e0e6622689b855f6f97e605f9302a011
JLAChaves <lucas2015nxi@gmail.com>
2022-03-04 22:54:39 -0400

    First controller and Razor pages tests

--- SalesWebMvc/Pages/About.cshtml.cs
+++ SalesWebMvc/Pages/About.cshtml.cs
@@ -9,12 +9,16 @@ namespace SalesWebMvc.Pages
     public class AboutModel : PageModel
     {
         public string Message { get; set; }
+        public string Professor { get; set; }
         public string Student { get; set; }
+        public string Email { get; set; }
 
         public void OnGet()
         {
-            Message = "Salles Web MVC App from c# Course";
+            Message = "Salles Web MVC App from C# Course";
+            Professor = "Professor: Dr. Nelio Alves";
             Student = "Student: JLAChaves";
+            Email = "lucas2015nxi@gmail.com";
         }
     }
 }
